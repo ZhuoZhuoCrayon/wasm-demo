fc45761b9b4a585953b98a5f202ebbfbc31fa504
crayon <873217631@qq.com>
2024-03-31 12:05:13 +0800

    fix(plugins/http_data_extractor): fix some bug

--- src/plugins/http_data_extractor/main.go
+++ src/plugins/http_data_extractor/main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"io"
-	"strconv"
-
-	// 将 nottinygc 作为 TinyGo 编译 WASI 的一个替代内存分配器，默认的内存分配器在数据量大的场景会有性能问题
 	"github.com/deepflowio/deepflow-wasm-go-sdk/sdk"
 	"github.com/valyala/fastjson"
 	_ "github.com/wasilibs/nottinygc"
+	"io"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -20,16 +18,19 @@ const (
 
 var httpPathPrefixes = []string{
 	"/v3/r/mpay",
+	// "/api/checkout",
 }
 
 var expectDataFields = map[string]bool{
-	"openid":   true,
-	"err_code": true,
-	"ret":      true,
+	"openid": true,
+	// "err_code": true,
+	// "ret":      true,
+	// "userId":   true,
+	// "orderId":  true,
 }
 
 func isPathMatched(path string, prefixes []string) bool {
-	sdk.Info("[isPathMatched] path -> %s", path)
+	// sdk.Info("[isPathMatched] path -> %s", path)
 	for _, prefix := range prefixes {
 		if strings.HasPrefix(path, prefix) {
 			sdk.Info("[isPathMatched] path -> %s, matched -> %s", path, prefix)
@@ -41,15 +42,15 @@ func isPathMatched(path string, prefixes []string) bool {
 
 func extract(b io.ReadCloser, fields map[string]bool) ([]sdk.KeyVal, *sdk.Trace, error) {
 	body, err := io.ReadAll(b)
-	if err != nil {
+	if err != nil || len(body) == 0 {
 		return nil, nil, err
 	}
-	sdk.Info("[extract] body -> %#v", body)
+	// sdk.Info("[extract] body -> %#v", body)
 	fv, err := fastjson.ParseBytes(body)
 	if err != nil {
 		return nil, nil, err
 	}
-	attrs := make([]sdk.KeyVal, 0, len(expectDataFields))
+	attrs := make([]sdk.KeyVal, 0, len(fields))
 	for field := range fields {
 		if !fv.Exists(field) {
 			continue
@@ -75,18 +76,20 @@ type httpHook struct {
 func (p httpHook) HookIn() []sdk.HookBitmap {
 	return []sdk.HookBitmap{
 		sdk.HOOK_POINT_HTTP_REQ,
-		sdk.HOOK_POINT_HTTP_RESP,
+		// sdk.HOOK_POINT_HTTP_RESP,
 	}
 }
 
 func (p httpHook) OnHttpReq(ctx *sdk.HttpReqCtx) sdk.Action {
-	if ctx.BaseCtx.L7 != HTTP1 || !isPathMatched(ctx.Path, httpPathPrefixes) {
+	baseCtx := &ctx.BaseCtx
+	if baseCtx.L7 != HTTP1 || !isPathMatched(ctx.Path, httpPathPrefixes) {
 		return sdk.ActionNext()
 	}
-	payload, err := ctx.BaseCtx.GetPayload()
+	payload, err := baseCtx.GetPayload()
 	if err != nil {
 		return sdk.ActionAbortWithErr(err)
 	}
+	// sdk.Info("[OnHttpReq] payload -> %#v", payload)
 	req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(payload)))
 	if err != nil {
 		return sdk.ActionNext()
@@ -99,22 +102,26 @@ func (p httpHook) OnHttpReq(ctx *sdk.HttpReqCtx) sdk.Action {
 }
 
 func (p httpHook) OnHttpResp(ctx *sdk.HttpRespCtx) sdk.Action {
-	if ctx.BaseCtx.L7 != HTTP1 {
-		return sdk.ActionNext()
-	}
-	payload, err := ctx.BaseCtx.GetPayload()
-	if err != nil {
-		return sdk.ActionAbortWithErr(err)
-	}
-	resp, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(payload)), nil)
-	if err != nil {
-		return sdk.ActionNext()
-	}
-	attrs, trace, err := extract(resp.Body, expectDataFields)
-	if err != nil {
-		return sdk.ActionNext()
-	}
-	return sdk.HttpReqActionAbortWithResult(nil, trace, attrs)
+	return sdk.ActionNext()
+	// TODO(crayon) Response 计算量太大，暂不开启
+	//baseCtx := &ctx.BaseCtx
+	//if baseCtx.L7 != HTTP1 {
+	//	return sdk.ActionNext()
+	//}
+	//payload, err := baseCtx.GetPayload()
+	//if err != nil {
+	//	return sdk.ActionAbortWithErr(err)
+	//}
+	//// sdk.Info("[OnHttpResp] payload -> %#v", payload)
+	//resp, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(payload)), nil)
+	//if err != nil {
+	//	return sdk.ActionNext()
+	//}
+	//attrs, trace, err := extract(resp.Body, expectDataFields)
+	//if err != nil {
+	//	return sdk.ActionNext()
+	//}
+	//return sdk.HttpReqActionAbortWithResult(nil, trace, attrs)
 }
 
 func (p httpHook) OnCheckPayload(baseCtx *sdk.ParseCtx) (uint8, string) {
